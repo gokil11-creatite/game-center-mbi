618aa9a974933ea79b1c09398a493acec8616ece
gokil11-creatite <denykurniawan478@gmail.com>
2025-11-20 13:10:27 +0700

    Add files via upload

---
+++ game/matematika-ninja/game.js
@@ -0,0 +1,8 @@
+// SOUND GAME 1 - Matematika Ninja
+const clickSound = new Audio("../../sound/game1/click.mp3");
+const winSound   = new Audio("../../sound/game1/win.mp3");
+const loseSound  = new Audio("../../sound/game1/lose.mp3");
+
+function sndClick() { clickSound.play(); }
+function sndWin()   { winSound.play(); }
+function sndLose()  { loseSound.play(); }
